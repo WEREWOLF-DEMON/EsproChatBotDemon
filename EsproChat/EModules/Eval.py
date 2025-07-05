8a826e7b47cc8c10aeacfa349a3c635952527bdc
RitikRohin <irthaiwan@gmail.com>
2025-07-06 01:18:51 +0530

    ***

--- EsproChat/EModules/Eval.py
+++ EsproChat/EModules/Eval.py
@@ -5,7 +5,7 @@ import sys
 import asyncio
 import traceback
 from inspect import getfullargspec
-from io import utilIO
+from io import StringIO
 from Murali import Owner
 from time import time
 from pyrogram import filters
@@ -52,8 +52,8 @@ async def executor(client: app, message: Message):
     t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = utilIO()
-    redirected_error = sys.stderr = utilIO()
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
     try:
         await aexec(cmd, client, message)
