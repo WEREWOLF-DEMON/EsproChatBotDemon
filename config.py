b7130d57c7494d44590f2c15b709df39b98b18e4
RitikRohin <irthaiwan@gmail.com>
2025-07-05 21:26:46 +0530

    ***

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ SETCMD = getenv("SETCMD", "True")
 # upstream repo 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/EsproSupport/appChatBot",
+    "https://github.com/RitikRohin/EsproChatBot",
 )
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
