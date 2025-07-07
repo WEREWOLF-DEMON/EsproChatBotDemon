1ac4b76fdd07612d9732ae877c45318551999741
RitikRohin <irthaiwan@gmail.com>
2025-07-07 09:11:33 +0530

    Update __main__.py

--- EsproChat/__main__.py
+++ EsproChat/__main__.py
@@ -1,40 +1,38 @@
 import asyncio
 import importlib
-import traceback
 from pyrogram import idle
-from pyrogram.types import BotCommand
 from EsproChat import app
+from pyrogram.types import BotCommand
 from EsproChat.EModules import ALL_EModules
 from config import LOGGER_ID, SETCMD
 
+loop = asyncio.get_event_loop()
+
+#all Fixed
+
 async def Murali():
-    await app.start()
+await app.start()
+for all_module in ALL_EModules:
+importlib.import_module("EsproChat.EModules." + all_module)
+print("Start ChatBot MissEspro ✨")
+print("Join Karo @EsproUpdate")
+if SETCMD:
+try:
+await app.set_bot_commands(
+[
+BotCommand("alive", "ᴄʜᴇᴄᴋ ʙᴏᴛ ɪꜱ ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ"),
+BotCommand("id", "ᴄʜᴇᴄᴋ ʏᴏᴜʀ ɪᴅ"),
+BotCommand("ping", "ᴄʜᴇᴄᴋ ʙᴏᴛ ᴘɪɴɢ"),
+BotCommand("chatbot", "ᴇɴᴀʙʟᴇ ᴏʀ ᴅɪꜱᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ "),
+BotCommand("start", "ꜱᴛᴀʀᴛ ᴄʜɪᴋᴜ ʙᴏᴛ"),
+BotCommand("tgm", "ᴜᴘʟᴏᴀᴅ ꜰɪʟᴇ ᴛᴏ ᴛᴇʟᴇɢʀᴀᴘʜ ")
+]
+)
+except Exception as e:
+print(f"Failed to set bot commands: {e}")
+pass
+await idle()
 
-    for all_module in ALL_EModules:
-        try:
-            importlib.import_module("EsproChat.EModules." + all_module)
-        except Exception as err:
-            print(f"Failed to import {all_module}: {err}")
+if name == "main":
+loop.run_until_complete(Murali())
 
-    print("ESPRO BOT HAS BEEN STARTED")
-    print("Join our Channel: @EsproUpdate")
-
-    if SETCMD:
-        try:
-            await app.set_bot_commands(
-                [
-                    BotCommand("alive", "check bot is alive or dead"),
-                    BotCommand("id", "check your id"),
-                    BotCommand("ping", "check bot ping"),
-                    BotCommand("chatbot", "enable or disable chatbot"),
-                    BotCommand("start", "start chiku bot"),
-                    BotCommand("tgm", "upload file to telegra.ph")
-                ]
-            )
-        except Exception:
-            traceback.print_exc()
-
-    await idle()
-
-if __name__ == "__main__":
-    asyncio.run(Murali())
